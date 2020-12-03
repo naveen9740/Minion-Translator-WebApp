74972fceef2e7cfc2151322337843b8570069462
NAVEEN KAMATH <naveen.kamath002@gmail.com>
2020-12-04 00:53:27 +0530

    initial commits

---
+++ index.js
@@ -0,0 +1,33 @@
+let btnTranslate=document.querySelector('#btn-translate');
+
+let txtInput=document.querySelector('.txt-input');
+console.log(txtInput);
+
+let outputDiv=document.querySelector('#output');
+console.log(outputDiv);
+
+let serverUrl="https://api.funtranslations.com/translate/minion.json";
+
+function getTranslatedUrl(txt){
+    return serverUrl+"?"+"text="+txt;
+}
+function errorHandler(error){
+    console.log("Error Occured"+error);
+    alert("The error is"+error);
+}
+
+function clickEventHandler(){
+    console.log('clicked');
+    console.log("Input: ",txtInput.value);
+
+    fetch(getTranslatedUrl(txtInput.value))
+    .then(response=>response.json())
+    .then(json=>{
+        var transText=json.contents.translated;
+        outputDiv.innerText=transText;
+    })
+    .catch(errorHandler)
+}
+
+btnTranslate.addEventListener('click',clickEventHandler);
+console.log(btnTranslate);
